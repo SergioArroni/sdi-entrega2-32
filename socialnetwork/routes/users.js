e8eed6d84ecc68239d45f249fc7319614162d3b8
Sergio <uo276341@uniovi.es>
2022-04-20 00:31:26 +0200

    Ejercicios 1, 2 y 3 acabados

--- socialnetwork/routes/users.js
+++ socialnetwork/routes/users.js
@@ -7,7 +7,7 @@ module.exports = function (app, usersRepository) {
         res.send("El usuario se ha desconectado correctamente");
     })
     app.get('/users/login', function (req, res) {
-        res.render("users/register.twig");
+        res.render("users/login.twig");
     })
     app.post('/users/login', function (req, res) {
             let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
@@ -22,7 +22,11 @@ module.exports = function (app, usersRepository) {
                     res.redirect("/users/login" + "?message=Email o password incorrecto" + "&messageType=alert-danger ");
                 } else {
                     req.session.user = user.email;
-                    res.redirect("/publications");
+                    if(user.rol === "Admin"){
+                        res.redirect("/");
+                    }else{
+                        res.redirect("/");
+                    }
                 }
             }).catch(error => {
                 req.session.user = null;
@@ -30,21 +34,36 @@ module.exports = function (app, usersRepository) {
             })
         }
     )
-    app.get('/users/signup', function (req, res) {
-        res.render("users/login.twig");
+    app.get('/users/register', function (req, res) {
+        res.render("users/register.twig");
     })
-    app.post('/users/signup', function (req, res) {
-        if(req.body.password.equals(req.body.repeatPassword)) {
-            let securePassword = app.get("crypto").createHmac('sha256', app.get('clave')).update(req.body.password).digest('hex');
-            let user = {
-                email: req.body.email,name: req.body.name,surname: req.body.surname, password: securePassword
-            }
-            usersRepository.insertUser(user).then(userId => {
-                res.redirect("/users/login" + "?message=Nuevo usuario registrado" + "&messageType=alert-info ");
-            }).catch(error => {
-                res.redirect("/users/signup" + "?message=Se ha producido un error al registrar el usuario" + "&messageType=alert-danger ");
-            });
+    app.post('/users/register', function (req, res) {
+        let filter = {
+            email: req.body.email
         }
-        res.redirect("/users/signup" + "?message=Se ha producido un error al registrar el usuario" + "&messageType=alert-danger ");
+        let options = {};
+        usersRepository.findUser(filter, options).then(user => {
+            if (user == null) {
+                if (req.body.password === req.body.repeatPassword) {
+                    let securePassword = app.get("crypto").createHmac('sha256', app.get('clave')).update(req.body.password).digest('hex');
+                    let user = {
+                        email: req.body.email, name: req.body.name, surname: req.body.surname, password: securePassword
+                    }
+                    usersRepository.insertUser(user).then(userId => {
+                        res.redirect("/users/login");
+                    }).catch(error => {
+                        res.redirect("/users/register" + "?message=Se ha producido un error al registrar el usuario" + "&messageType=alert-danger ");
+                    });
+                } else {
+                    res.redirect("/users/register" + "?message=Las dos contraseñas no coinciden" + "&messageType=alert-danger ");
+                }
+            } else {
+                res.redirect("/users/register" + "?message=Este email ya esta vinculado con un usuario" + "&messageType=alert-danger ");
+            }
+        }).catch(error => {
+            req.session.user = null;
+            res.redirect("/users/login" + "?message=Se ha producido un error al buscar el usuario" + "&messageType=alert-danger ");
+        })
+
     });
 }
