d677b84099b2fd5edb66ea0cf46441fc103fff19
Hugo Gutiérrez Tomás <uo277891@uniovi.es>
2022-05-12 14:35:22 +0200

    arreglo test 24.

--- src/test/java/com/uniovi/sdi_entrega_2_32/SdiEntrega132ApplicationTests.java
+++ src/test/java/com/uniovi/sdi_entrega_2_32/SdiEntrega132ApplicationTests.java
@@ -15,13 +15,13 @@ class SdiEntrega132ApplicationTests {
     static String PathFirefox = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
             //"C:\\Program Files\\Mozilla Firefox\\firefox.exe";
     //static String Geckodriver ="C:\\nada.exe;
-    //static String GeckodriverHugo ="C:\\Users\\Hugo\\Desktop\\TERCER_CURSO_INGENIERIA\\SDI\\PRACTICA\\sesion06\\PL-SDI-Sesión5-material\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
+    static String GeckodriverHugo ="C:\\Users\\Hugo\\Desktop\\TERCER_CURSO_INGENIERIA\\SDI\\PRACTICA\\sesion06\\PL-SDI-Sesión5-material\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
 
-    static String GeckodriverAndrea = "C:\\Users\\andre\\Documents\\CURSO 2021-2022\\CUATRI 2\\SDI\\geckodriver.exe";
+    //static String GeckodriverAndrea = "C:\\Users\\andre\\Documents\\CURSO 2021-2022\\CUATRI 2\\SDI\\geckodriver.exe";
     //static String GeckodriverSergio = "C:\\Dev\\tools\\selenium\\geckodriver-v0.30.0-win64.exe";
 
     //Común a Windows y a MACOSX
-    static WebDriver driver = getDriver(PathFirefox, GeckodriverAndrea);
+    static WebDriver driver = getDriver(PathFirefox, GeckodriverHugo);
 
     static String URL = "http://localhost:8081";
 
@@ -448,7 +448,7 @@ class SdiEntrega132ApplicationTests {
     @Order(24)
     public void PR24() {
         PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
-        PO_LoginView.fillLoginForm(driver, "user08@email.com", "user03");
+        PO_LoginView.fillLoginForm(driver, "user04@email.com", "user04");
 
         // Entramos en la ventana de creación
         PO_PrivateView.click(driver, "//a[contains(@href, '/users/create/publication')]", 0);
